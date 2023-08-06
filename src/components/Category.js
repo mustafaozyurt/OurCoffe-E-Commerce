b580faa576e6befae2e7176a786dd97dc009c3bc
mustafaozyurt <mustafaozyurt38@gmail.com>
2023-08-06 19:21:41 +0300

    update Category.js

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,13 +1,6 @@
 import "./Category.css";
 
-const Category = ({ show, children, onClick, className }) => {
-
-  const categoriesMobileClass = ["categories__mobile"];
-
-  if (show) {
-    categoriesMobileClass.push("show");
-  }
-
+const Category = ({ children, onClick, className }) => {
   return (
     <div className={className} onClick={onClick}>
       {children}
